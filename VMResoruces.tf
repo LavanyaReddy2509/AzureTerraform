79657283df738569ed0cdc3a9fe4dee67fce4a92
LavanyaReddy2509 <105622875+LavanyaReddy2509@users.noreply.github.com>
2024-09-12 15:54:20 +0530

    Update VMResoruces.tf

--- VMResoruces.tf
+++ VMResoruces.tf
@@ -31,7 +31,7 @@ resource "azurerm_virtual_machine" "vm1" {
   }
 
   tags = {
-    "environment" = "Production"
+    "environment" = "production"
     "tier"        = "frontend"
   }
 }
@@ -71,6 +71,6 @@ resource "azurerm_public_ip" "public1" {
   allocation_method = "Static"
 
   tags = {
-    "environment" = "Production"
+    "environment" = "production"
   }
 }
