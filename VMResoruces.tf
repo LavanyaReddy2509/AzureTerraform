0f0f4a38cb91ee71f72188e06c996c6f52e4f502
LavanyaReddy2509 <105622875+LavanyaReddy2509@users.noreply.github.com>
2024-09-12 16:05:01 +0530

    Update VMResoruces.tf

--- VMResoruces.tf
+++ VMResoruces.tf
@@ -1,5 +1,5 @@
 resource "azurerm_virtual_machine" "vm1" {
-  name                  = "testetstetestvm9"
+  name                  = "testetstetestvm"
   resource_group_name   = "TerraformRG"
   location              = "East US"
   network_interface_ids = [azurerm_network_interface.nic1.id]
